c4d1cc3dd19df5216974ebe091c6cafb43262d0e
mkrebs <michael.krebs@ux5.edvschule-plattling.de>
2022-01-16 22:11:23 +0100

    Some Changes

--- src/packungsversand/FXMLDocumentController.java
+++ src/packungsversand/FXMLDocumentController.java
@@ -32,6 +32,7 @@ public class FXMLDocumentController implements Initializable {
     ArrayList<Button> buttons = new ArrayList<>();
     Alert alert = new Alert(Alert.AlertType.ERROR);
     private RadioButton rbPressed;
+
     // region importFXML
     @FXML
     private VBox vbArten;
