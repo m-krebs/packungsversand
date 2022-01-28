b292e1f8d21d066f842a5828d3c0576c8b72dd26
mkrebs <michael.krebs@edvschule-plattling.de>
2022-01-28 08:24:53 +0100

    Opened in Library

--- src/packungsversand/Packungsversand.java
+++ src/packungsversand/Packungsversand.java
@@ -2,13 +2,7 @@
 package packungsversand;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import sun.applet.Main;
-
 
 public class Packungsversand extends Application {
 
